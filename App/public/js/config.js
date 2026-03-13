0d53140c885c1ac7e8ebecacacb57d1b2367be0d
SynapLink <m@1leet.com>
2026-03-13 22:31:55 +0100

    Update config with GitHub repository details

--- App/public/js/config.js
+++ App/public/js/config.js
@@ -1,5 +1,5 @@
 const config = {
-    GITHUB_OWNER: 'YOUR_GITHUB_USERNAME_HERE',  // Change this!
-    GITHUB_REPO: 'YOUR_GITHUB_REPO_NAME_HERE',  // Change this!
-    OAUTH_CLIENT_ID: 'YOUR_GITHUB_OAUTH_CLIENT_ID', // Change this!
+    GITHUB_OWNER: 'SynapLink',  // Change this!
+    GITHUB_REPO: 'blob',  // Change this!
+    OAUTH_CLIENT_ID: '', // Change this!
 };
